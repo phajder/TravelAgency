b81d0bb99bf3fc70b573c556dab9f740640c1d6e
Piotrek <piootr.hajder@gmail.com>
2016-12-20 12:53:38 +0100

    Locale feature added.

---
+++ src/main/java/com/hajder/travelagency/bean/LocaleBean.java
@@ -0,0 +1,48 @@
+package com.hajder.travelagency.bean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * Created by pioot on 20.12.2016.
+ */
+@ManagedBean
+@SessionScoped
+public class LocaleBean {
+    private static Map<String, Object> locales;
+
+    private Locale locale;
+
+    static {
+        locales = new LinkedHashMap<>();
+        locales.put("Polski", new Locale("pl"));
+        locales.put("English", Locale.ENGLISH);
+    }
+
+    @PostConstruct
+    public void init() {
+        locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+    }
+
+    public Map<String, Object> getLocalesInMap() {
+        return locales;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void setLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+}
